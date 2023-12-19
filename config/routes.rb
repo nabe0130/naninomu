21172fd1ecbcff8392b44ea6f64d109417d5db02
Apple <apple@WY.local>
2023-12-19 23:01:40 +0900

    controllers作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+# ここにgamesコントローラーのnewアクションのルーティングを追加する
+get 'games/new'
   # Defines the root path route ("/")
   # root "articles#index"
 end
