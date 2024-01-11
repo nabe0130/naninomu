761028bfafe5f0bf7bbed82bff83bbddb56dcab4
Apple <apple@WY.local>
2024-01-11 16:04:57 +0900

    ログイン機能

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Naninomu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7
+
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # GamesControllerのnewアクションをアプリケーションのメインページに設定
   root 'games#new'
 
