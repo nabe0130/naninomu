4172e1e64a1d76c352658d9085d8698e192a56d7
Apple <apple@WY.local>
2024-01-20 23:16:15 +0900

    結果画面の詳細画面修正3

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,4 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
+  belongs_to :cocktail # ここでカクテルモデルへの関連付けを行う
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :bookmarks # Bookmarksと関連付け
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
