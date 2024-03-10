4d454d04bb514edee8c74448a8b48160c4a00af0
Apple <apple@WY.local>
2024-03-11 08:31:43 +0900

    メールアドレス代替

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,8 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      # ※deviseのuserカラムに nameやprofile を追加している場合は下のコメントアウトを外して使用
-
       user.name = auth.info.name
-      # user.profile = auth.info.profile
-      user.email = auth.info.email
+      user.email = auth.info.email.presence || "#{auth.uid}@example.com"
       user.password = Devise.friendly_token[0, 20]
     end
   end
