a0fc15947b9fa066d630f94875c3b03b19eb41ab
Apple <apple@WY.local>
2024-03-11 05:22:08 +0900

    CSRF対策

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -49,34 +49,7 @@ module Users
     end
 
     def failure
-      redirect_to root_path
+      redirect_to new_user_session_path, alert: '外部サービスを通じた認証に失敗しました。'
     end
   end
 end
-
-# You should configure your model like this:
-# devise :omniauthable, omniauth_providers: [:twitter]
-
-# You should also create an action method in this controller like this:
-# def twitter
-# end
-
-# More info at:
-# https://github.com/heartcombo/devise#omniauth
-
-# GET|POST /resource/auth/twitter
-# def passthru
-#   super
-# end
-
-# GET|POST /users/auth/twitter/callback
-# def failure
-#   super
-# end
-
-# protected
-
-# The path used when OmniAuth fails
-# def after_omniauth_failure_path_for(scope)
-#   super(scope)
-# end
